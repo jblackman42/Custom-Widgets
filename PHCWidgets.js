09a3e02890ef8674f503e20b6385fda23350b9a3
jblackman42 <jblackman@pureheart.org>
2024-01-26 21:59:07 -0700

    initial commit

---
+++ PHCWidgets.js
@@ -0,0 +1,23 @@
+import { render, html } from "./util/preactCentral.js";
+
+import widgetsRegistry from "./widgets/index.js";
+
+(() => {
+  console.log("does this run?");
+  widgetsRegistry.forEach((widget) => {
+    const { name, component } = widget;
+
+    document.querySelectorAll(name).forEach((elem) => {
+      // Create an object to hold the attributes
+      const props = {};
+
+      // Loop through attributes and add them to the props object
+      for (const attribute of elem.attributes) {
+        props[attribute.name] = attribute.value;
+      }
+
+      // Render the component with the collected attributes as props
+      render(html`<${component} ...${props} />`, elem);
+    });
+  });
+})();
---
+++ widgets/index.js
@@ -0,0 +1,14 @@
+import Counter from "./Counter.js";
+import PHCLogin from "./PHCLogin.js";
+// import { SeriesFinder, SeriesDetails } from "./SeriesWidgets.js";
+
+export default [
+  {
+    name: "counter",
+    component: Counter,
+  },
+  {
+    name: "phc-user-login",
+    component: PHCLogin,
+  },
+];
